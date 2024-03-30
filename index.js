28ac7ebc03bfa26e42e33e2e476c9431440da982
Glitch (obvious-sparkling-walk) <none>
2024-03-30 16:10:35 +0000

    🚖😋 Checkpoint ./index.js:60282313/688

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bp=require("body-parser")
+// Options for dns.lookup() method
+const options = {
+ all: true, // Return all resolved addresses
+};
+
+// Calling dns.lookup() for a hostname and displaying the results in the console
+dns.lookup('example.com', options, (err, addresses) => {
+ if (err) {
+    console.error('Error:', err);
+ } else {
+    console.log('addresses:', addresses);
+ }
+});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bp.json({extended:true}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -15,8 +30,9 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  //make shorted version
+  console.log(req.body)
 });
 
 app.listen(port, function() {
